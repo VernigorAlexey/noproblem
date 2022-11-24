3fe7248b8d7befb21ffc0d9714fb757b65c786e5
VernigorAlexey <veroil@inbox.ru>
2022-11-24 20:20:56 +0500

    Example_007_BASE

---
+++ Example_007_BASE/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = 11;
+int b = 3;
+int c = 5;
+int d = 8;
+int e = 78;
+
+int max = a;
+
+if (a>max) max = a;
+if (b>max) max = b;
+if (c>max) max = c;
+if (d>max) max = d;
+if (e>max) max = e;
+
+Console.Write ("Max = ");
+Console.WriteLine (max);
+
+    
+
