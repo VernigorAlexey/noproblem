8b3f65275ed03e9bdb687522063401b1ae122c80
VernigorAlexey <veroil@inbox.ru>
2022-11-24 20:00:15 +0500

    Example_006_OLA

---
+++ Example_006_OLA/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine ("Введите имя пользователя:  ");
+string username = Console.ReadLine ();
+
+if (username.ToLower () == "оля")
+
+{ 
+     Console.WriteLine ("Ура! Это ОЛЯ!!!");
+
+}
+
+else
+
+{ Console.Write ("Привет,");
+Console.WriteLine (username);
+
+}
