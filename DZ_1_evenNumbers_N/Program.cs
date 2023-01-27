c5b2c06e69922e98bb11088a857df61d94a5cac8
VernigorAlexey <veroil@inbox.ru>
2023-01-27 17:35:18 +0500

    DZ_1_evenNumbers_N

---
+++ DZ_1_evenNumbers_N/Program.cs
@@ -0,0 +1,23 @@
+﻿// Показать четные числа от 1 до N
+
+
+Console.WriteLine("Введите целое число (N). А мы покажем все чётные числа от 1 до N ");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = 2;
+
+if (n >= m)
+{
+         while (m <= n)
+        {
+            Console.Write($" {m}");
+            m = m + 2;
+        }
+
+    }
+
+
+else
+{
+    Console.Write("Нет чисел. Введите другое число!");
+
+}
