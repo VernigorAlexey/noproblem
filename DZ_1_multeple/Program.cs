2630dbd6822793c40af81c4139a9c203b1a64360
VernigorAlexey <veroil@inbox.ru>
2023-01-27 22:46:18 +0500

    DZ_1_multeple

---
+++ DZ_1_multeple/Program.cs
@@ -0,0 +1,20 @@
+﻿// Выяснить, кратно ли число заданному, если нет, вывести остаток.
+
+
+Console.WriteLine ("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+int c = a % b;
+
+if (c==0)
+
+{
+    Console.WriteLine ($"Первое число {a} кратно {b}");
+}
+
+else
+
+{
+    Console.WriteLine($"Первое число {a} не кратно {b}, остаток {c}!");
+}
