8ed0733066fff5329e50df8edc69b6c5ecb9b6bf
VernigorAlexey <veroil@inbox.ru>
2023-01-27 17:16:16 +0500

    DZ_1_evenNumber

---
+++ DZ_1_evenNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿// Выяснить является ли число чётным
+
+
+Console.Write("Введите число чтобы выяснить, является ли оно четным:   ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine($"Число {a} является четным");
+}
+
+else
+{
+     Console.WriteLine($"Число {a} не является четным");
+}
