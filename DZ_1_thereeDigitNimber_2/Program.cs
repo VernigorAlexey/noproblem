53a5f491e37f9ad73ad58072aa8aa9708f591872
VernigorAlexey <veroil@inbox.ru>
2023-01-27 19:59:20 +0500

    DZ_1_threeDigitNumber_2

---
+++ DZ_1_thereeDigitNimber_2/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать вторую цифру трёхзначного числа
+
+Console.WriteLine("Введите целое трехзначное число, а мы покажем его вторую цифру");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Math.Abs (n);
+int b = 99;
+int c = 1000;
+int a = m/10 % 10;
+
+if (m > b && m < c)
+
+{
+          
+            Console.WriteLine($"Вторая цифра числа {a}");
+           
+        }
+
+
+else
+{
+    Console.Write("Введите трехзначное число!");
+}
