e5833780790ff1c46822cf8f3e9469ea193c2a11
VernigorAlexey <veroil@inbox.ru>
2023-01-27 18:22:05 +0500

    DZ_1_threeDigitNumber_3

---
+++ DZ_1_threeDigitNumber_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Показать последнюю цифру трёхзначного числа
+
+Console.WriteLine("Введите целое трехзначное число, а мы покажем его последнюю цифру");
+int n = Convert.ToInt32(Console.ReadLine());
+int m = Math.Abs (n);
+int b = 99;
+int c = 1000;
+int a = m % 10;
+
+if (m > b && m < c)
+
+{
+          
+            Console.WriteLine($"Последняя цифра числа {a}");
+           
+        }
+
+
+else
+{
+    Console.Write("Введите трехзначное число!");
+}
