5ad9a7965d2e59e068023e1e6426cea3645f5ed5
VernigorAlexey <veroil@inbox.ru>
2023-01-27 16:04:04 +0500

    DZ_1_max_3_numbers

---
+++ DZ_1_max_3_nambers/Program.cs
@@ -0,0 +1,14 @@
+﻿// Найти максимальное из трех чисел
+
+
+Console.WriteLine ("Введите три числа и мы определим максимальное из них");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int c = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+if     (b>max) max = b;
+else if (c>max) max = c;
+
+Console.Write ($"Максимальное число {max} ");
+
