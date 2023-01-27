b7dbee96825a02f392c4ca1882da3d2b3197f4d6
VernigorAlexey <veroil@inbox.ru>
2023-01-27 23:37:10 +0500

    DZ_1_12

---
+++ DZ_1_12/Program.cs
@@ -0,0 +1,26 @@
+﻿// Найти третью цифру числа или сообщить, что её нет
+
+
+Console.WriteLine("Возьмем случайное число и определим его третью цифру, или поймем что её нет! ");
+int n = new Random().Next(0, 1000000);
+//int n = Convert.ToInt32(Console.ReadLine());  для ручной проверкм
+Console.WriteLine($"Случайное число : {n} ");
+
+
+if (n > 99)
+
+{
+    while (n > 999)  n = n / 10;
+    n = n % 10; 
+   
+
+    Console.WriteLine($"Третья цифра : {n} ");
+
+
+}
+
+else
+
+{
+    Console.WriteLine("Третьей цифры нет");
+}
