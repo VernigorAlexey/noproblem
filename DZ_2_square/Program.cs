f1e3d4c5ca506cf2deed2e2c7b671a416f2e506a
VernigorAlexey <veroil@inbox.ru>
2023-01-31 20:05:21 +0500

    DZ_square

---
+++ DZ_2_square/Program.cs
@@ -0,0 +1,27 @@
+﻿// По двум заданным числам проверять является ли одно квадратом другого
+
+Console.WriteLine("По двум заданным числам проверим является ли одно квадратом другого");
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+
+if (a*a == b)
+
+{
+    Console.WriteLine($"Второе число {b} является квадратом первого числа {a} ");
+}
+
+else if (b*b == a)
+
+{
+    Console.WriteLine($"Первое число {a} является квадратом второго числа {b} ");
+}
+
+else
+
+{
+    Console.WriteLine("Числа не являются квадратами друг для друга");
+}
+
