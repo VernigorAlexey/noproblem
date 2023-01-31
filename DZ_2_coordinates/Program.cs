8de8e45e3bf8ece7aa2386a530548c09cc04099f
VernigorAlexey <veroil@inbox.ru>
2023-01-31 21:47:21 +0500

    DZ_2_coordinates

---
+++ DZ_2_coordinates/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Задайте номер четверти (от 1 до 4) Декартовой системы координат, покажем диапазоны для возможных координат");
+int quarter = Convert.ToInt32(Console.ReadLine());
+
+if (quarter == 1)
+{
+    Console.WriteLine($"Для четверти {quarter} возможны координаты: X>0,Y>0");
+
+}
+
+else if (quarter == 2)
+{
+    Console.WriteLine($"Для четверти {quarter} возможны координаты: X<0,Y>0");
+
+}
+
+else if (quarter == 3)
+{
+    Console.WriteLine($"Для четверти {quarter} возможны координаты: X<0,Y<0");
+
+}
+
+else if (quarter == 4)
+{
+    Console.WriteLine($"Для четверти {quarter} возможны координаты: X>0,Y<0");
+
+}
+
+else
+{
+    Console.WriteLine("Неправильно задана четверть!");
+
+}
