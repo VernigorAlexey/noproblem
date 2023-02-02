eaa07869b348f85a18fec16ba64387cd3c512c61
VernigorAlexey <veroil@inbox.ru>
2023-02-03 00:16:40 +0500

    S1

---
+++ S1/Program.cs
@@ -0,0 +1,11 @@
+﻿
+Console.Write("Введите первое число:");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите стерень числa:");
+int y = Convert.ToInt32(Console.ReadLine());
+
+
+double z = Math.Pow(x, y);
+
+Console.Write($"Число {x} в степени {y} равно {z}:");
