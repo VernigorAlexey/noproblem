40d2b790a2172adcce3f55ced8e7377108cda67f
VernigorAlexey <veroil@inbox.ru>
2023-02-04 13:41:11 +0500

    DZ_3_cube

---
+++ DZ_3_cube/Program.cs
@@ -0,0 +1,25 @@
+﻿// Найти кубы чисел от 1 до N
+
+
+Console.WriteLine("Введите число N. Покажем кубы всех чисел от 1 до N включительно.");
+int n = int.Parse(Console.ReadLine());
+//double z = 0;  
+//double y = 3;   второй вариант решения
+
+Console.WriteLine("Кубы всех чисел от 1 до N.");
+
+if (n > 0)
+{
+    for (int i = 1; i <= n; i++)
+
+    {
+        Console.WriteLine($"{i} \t {i * i * i}");
+        //Console.WriteLine($"{i} \t {z = Math.Pow(i, y)}");    второй вариант решения
+    }
+
+}
+else
+
+{
+    Console.WriteLine("Введите число больше ноля!");
+}
