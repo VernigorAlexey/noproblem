cceaa609b9fb5610900efd81fc36382134197178
VernigorAlexey <veroil@inbox.ru>
2023-02-04 15:40:05 +0500

    DZ_3_sumNumbers

---
+++ DZ_3_sumNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿// Подсчитать сумму цифр в числе
+
+Console.WriteLine("Введите число. Робот сосчитает сумму его чисел =)");
+int n = int.Parse(Console.ReadLine());
+int c = n; // для печати ответа
+n = Math.Abs(n);
+
+int m = 0;
+
+while (n > 0)
+{
+    m = m + n % 10;
+    n = n / 10;
+}
+
+Console.WriteLine($"Сумма цифр числа {c} равна {m}");
+
+
