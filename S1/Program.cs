4ed4d3a7ecfd4a2a08eff5b7fb967654f84823b3
VernigorAlexey <veroil@inbox.ru>
2023-02-05 11:32:01 +0500

    S1

--- S1/Program.cs
+++ S1/Program.cs
@@ -5,7 +5,13 @@ int x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите стерень числa:");
 int y = Convert.ToInt32(Console.ReadLine());
 
+int z = 1;
+// double z = Math.Pow(x, y);
 
-double z = Math.Pow(x, y);
+for (int i=1; i<=y; i++)
+{
+    z = x*z;
+    // z *=x;
+}
 
 Console.Write($"Число {x} в степени {y} равно {z}:");
