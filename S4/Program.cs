65398800f3a7fd6089f9e8b3cb26e9b0ee37bd78
VernigorAlexey <veroil@inbox.ru>
2023-02-07 00:23:47 +0500

    S4_12

--- S4/Program.cs
+++ S4/Program.cs
@@ -29,3 +29,39 @@ while (i < array.Length)
 
 Console.WriteLine($"Сумма положительных элементов массива равна {result1}");
 Console.WriteLine($"Сумма отрицательных элементов массива равна {result2}");
+
+
+
+void PrintArray(int[] array)
+{
+    foreach(int element in array)
+        Console.Write($"{element} ");
+    Console.WriteLine();
+}
+int[] CreateArray(int length, int lowest, int hightest)
+{
+    Random random = new Random();
+    int[] array = new int[length];
+    for (int i = 0; i < array.Length; i++)
+        array[i] = random.Next(lowest, hightest + 1);
+    return array;
+}
+bool Constains(int[] array, int value)
+{
+    foreach(int element in array)
+        if (element == value)
+            return true;
+    return false;
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+int length = ReadInt("Enter size of array: ");
+int lowest = ReadInt("Enter left part of array range: ");
+int hightest = ReadInt("Enter right part of array range: ");
+int[] array = CreateArray(length, lowest, hightest);
+PrintArray(array);
+int value = ReadInt("Enter element to find: ");
+Console.WriteLine(Constains(array, value) ? "There is element into array" : "There is no element into array" );
