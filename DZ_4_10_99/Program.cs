9807d133109485fd6aed06db1197c41c021c7858
VernigorAlexey <veroil@inbox.ru>
2023-02-08 23:16:35 +0500

    DZ_4_10_99

---
+++ DZ_4_10_99/Program.cs
@@ -0,0 +1,15 @@
+﻿// В одномерном массиве из 123 чисел найти количество элементов из отрезка [10,99]
+
+Console.Write("Введите первое число отрезка: ");
+int a = int.Parse(Console.ReadLine());  // 10
+Console.Write("Введите второе число отрезка: ");
+int b = int.Parse(Console.ReadLine());  // 99
+int count = 0;
+
+for (int i = a; i < b+1; i++)
+{
+
+    count++;
+
+}
+Console.WriteLine($"Количество элементов из отрезка [{a},{b}] равно {count}"); // 90
