6612b5727cbc1e123c615508a8981cf067678010
VernigorAlexey <veroil@inbox.ru>
2023-02-12 22:03:13 +0500

    DZ_4_5

---
+++ DZ_4_5/Program.cs
@@ -0,0 +1,27 @@
+﻿// Найти произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д.
+
+Console.Write("Введите количество элементов массива: ");
+int q = int.Parse(Console.ReadLine());
+int[] array = new int[q];
+
+
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0, 10);
+        Console.Write($"{array[i]} \t");
+    }
+}
+Console.WriteLine();
+
+int vork = 0;
+for (int i = 0; i < array.Length / 2; i++)
+{
+    vork = array[i] * array[array.Length - 1 - i];
+
+    Console.WriteLine($"Произведение пар чисел {i} - {array.Length - 1 - i} в одномерном массиве:{vork}");
+}
+
+
+
+
