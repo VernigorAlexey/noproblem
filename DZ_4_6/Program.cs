bf01d67f274b93bcf367305519ead969b8858dcf
VernigorAlexey <veroil@inbox.ru>
2023-02-16 20:36:51 +0500

    DZ_5_1

---
+++ DZ_4_6/Program.cs
@@ -0,0 +1,88 @@
+﻿// В Указанном массиве вещественных чисел найдите разницу между максимальным и минимальным элементом
+Console.Write("Введите количество элементов массива: ");
+int q = int.Parse(Console.ReadLine()!);
+
+double[] array = new double [q];
+ Random rand = new();
+// double min = 10.0;
+//  double max = 0.0;
+// {
+  for (int i = 0; i < array.Length; i++)
+  {
+      array[i]  = rand.Next(0, 10) + rand.NextDouble();
+ ;
+     Console.WriteLine($"{array[i]} \t");
+
+  }
+
+//     }
+// }
+// Console.WriteLine();
+
+// int vork = 0;
+// for (int i = 0; i < array.Length / 2; i++)
+// {
+//     vork = array[i] * array[array.Length - 1 - i];
+
+//     Console.WriteLine($"Произведение пар чисел {i} - {array.Length - 1 - i} в одномерном массиве:{vork}");
+// }
+
+
+
+
+
+// // Дан целочисленный массив. Найти среднее арифметическое каждого из столбцов.
+
+// int ReadInt(string text)
+//     {
+//         Console.WriteLine(text);
+//         return int.Parse(Console.ReadLine()!);
+//     }
+
+// void FillArray (int [,] array)
+//     {
+//         Random rnd = new Random();
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//                 array[i,j] = rnd.Next(0,10);
+//         }
+//     }
+
+// void PrintArray(int [,] array, string text)
+//     {
+//         Console.WriteLine("\n");
+//         Console.WriteLine(text);
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//                 if (array[i,j] >= 0)
+//                     Console.Write($" {array[i,j]} \t");
+//                 else
+//                     Console.Write($"{array[i,j]} \t");
+//         Console.WriteLine();
+//         }
+//     }
+
+
+
+// int MeanRows(int [,] array)
+// { 
+//     int count = 0;
+//     int sumTotal = 0;
+//     int sumRow = 0;
+//         for (int i = 0; i < array.GetLength(0); i++)
+//         {
+//             for (int j = 0; j < array.GetLength(1); j++)
+//                 sumRow += array[i,j];
+//         sumTotal += sumRow;
+//         sumRow = 0;
+//         count++;
+//         }
+//     return sumTotal/count;
+// }
+// int [,] numbers = new int[3,3];
+
+// FillArray(numbers);
+// PrintArray(numbers, "Оригинальный массив: ");
+// Console.WriteLine(MeanRows(numbers));
---
+++ DZ_5_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// // Найти точку пересечения двух прямых заданных уравнением y = k1 * x + b1, y = k2 * x + b2, b1 k1 и b2 и k2 заданы
+
+Console.WriteLine ("Введите k и b двух прямых, найдем точку пересечения.");
+void CrossPoint(double k1, double b1, double k2, double b2)
+{
+    double x = 0;
+    double y = 0;
+    {
+
+        if ((k1 == k2 && b1 == b2))
+
+        {
+            Console.Write("Заданные прямые совпадают!");
+
+
+        }
+        else if (k1 == k2)
+        {
+            Console.Write("Заданные прямые паралельны!");
+        }
+
+        else
+        {
+            x = (b1 - b2) / (k2 - k1);
+            y = (k2 * b1 - k1 * b2) / (k2 - k1);
+
+            Console.Write($"Точка пересечения заданных прямых: ({x} ; {y})");
+        }
+    }
+}
+
+Console.WriteLine("Введите b1: ");
+double bb = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k1: ");
+double kk = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите b2: ");
+double bbb = double.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите k2: ");
+double kkk = double.Parse(Console.ReadLine()!);
+
+CrossPoint(kk, bb, kkk, bbb);
