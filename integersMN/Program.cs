671278ee6ffd967496622817fdea2f28b1c32bbf
VernigorAlexey <veroil@inbox.ru>
2023-03-20 22:01:55 +0500

    integersMN

---
+++ integersMN/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите начальное значение M:");
+int m = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите конечное значение N:");
+int n = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Натуральные числа от M до N:");
+for (int i = m; i <= n; i++)
+{
+    Console.Write(i + " ");
+}
