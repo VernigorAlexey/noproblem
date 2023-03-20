769aa43046d3a1da587d395838de32fb466f241a
VernigorAlexey <veroil@inbox.ru>
2023-03-20 21:12:00 +0500

    firstN

---
+++ firstN/Program.cs
@@ -0,0 +1,19 @@
+﻿// Написать программу показывающие первые N чисел, для которых каждое следующее равно сумме двух предыдущих. 
+// Первые два элемента последовательности задаются пользователем
+
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+Console.Write("Введите количество чисел: ");
+int n = int.Parse(Console.ReadLine()!);
+int result = 0;
+Console.Write($"{firstNumber} {secondNumber} ");
+for (int i = 3; i <= n; i++)
+{
+    result = firstNumber + secondNumber;
+    Console.Write($"{result} ");
+    firstNumber = secondNumber;
+    secondNumber = result;
+}
+Console.WriteLine();
