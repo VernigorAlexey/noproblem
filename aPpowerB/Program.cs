6b0436ae406273e57a172325b7a022321c4e7e5a
VernigorAlexey <veroil@inbox.ru>
2023-03-20 21:50:19 +0500

    aPowerB

---
+++ aPpowerB/Program.cs
@@ -0,0 +1,11 @@
+﻿// Написать программу возведения числа А в целую стень B
+Console.Write("Введите число А: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень B: ");
+int b = int.Parse(Console.ReadLine()!);
+int result = 1;
+for (int i = 0; i < b; i++)
+{
+    result *= a;
+}
+Console.WriteLine($"Результат: {result}");
