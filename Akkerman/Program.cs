3053706d461bc78e0bb1eb26263276a10e249485
VernigorAlexey <veroil@inbox.ru>
2023-03-20 20:35:07 +0500

    Akkerman

---
+++ Akkerman/Program.cs
@@ -0,0 +1,32 @@
+﻿// Написать программу вычисления функции Аккермана
+
+
+int Akkerman(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return Akkerman(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return Akkerman(m - 1, Akkerman(m, n - 1));
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+    Console.WriteLine("Введите значения m и n для функции Аккермана:");
+    Console.Write("m = ");
+    int m = int.Parse(Console.ReadLine()!);
+    Console.Write("n = ");
+    int n = int.Parse(Console.ReadLine()!);
+    Console.WriteLine($"A({m}, {n}) = {Akkerman(m, n)}");
+
+
+
