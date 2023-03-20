45712d2ea8fa1e78108e2c2f3dbd7ddc861dfecc
VernigorAlexey <veroil@inbox.ru>
2023-03-20 21:27:13 +0500

    sumNumber

---
+++ sumNumber/Program.cs
@@ -0,0 +1,12 @@
+﻿// Найти сумму цифр числа
+int number, sum = 0;
+Console.Write("Введите целое число: ");
+number = int.Parse(Console.ReadLine()!);
+
+while (number != 0)
+{
+    sum += number % 10;
+    number /= 10;
+}
+
+Console.WriteLine("Сумма цифр числа: {0}", sum);
